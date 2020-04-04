e5cced5a2a03904174f2deaa90deefbbcc9629da
Antonio <Tony9706-@hotmail.com>
2020-04-04 09:37:34 -0600

    04/Abril/2020
    Implementacion de validacioón de registro de usuario Fulll(Validación+)

--- MiResidencia20/Views/Logueo/RegistroPage.xaml.cs
+++ MiResidencia20/Views/Logueo/RegistroPage.xaml.cs
@@ -14,9 +14,22 @@ namespace MiResidencia20.Views
 
         async void  Btn_Registrar(System.Object sender, System.EventArgs e)
         {
+            var usuario = UsuarioEntry.Text;
+            var carrera = carreraEntry.Text;
+            var grupo = grupoEntry.Text;
+            var periodo = periodoEntry.Text;
+
+            if(usuario == null || carrera == null ||  grupo == null || periodo == null)//Validaciones de campos vacíos
+            {
+                await DisplayAlert("Erro al registrarse", "Llene todos los campos!", "ok");
+            }
+            else
+            {
                 await DisplayAlert("Felicitaciones", "¡Usuario creado exitosamente!", "Comenzar");
                 await Navigation.PushAsync(new MenuTabbedPage());
-          
+
+            }
+
         }
     }
      
