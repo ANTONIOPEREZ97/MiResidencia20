373f4e4672e548b4c8a1f662a6400e31e61b91f5
Antonio <Tony9706-@hotmail.com>
2020-09-15 17:45:15 -0500

    15/Septiembre/2020
    Implementacion de SQLite

--- MiResidencia20/App.xaml.cs
+++ MiResidencia20/App.xaml.cs
@@ -2,11 +2,35 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using MiResidencia20.Views;
+using MiResidencia20.Data;
+using System.IO;
 
 namespace MiResidencia20
 {
     public partial class App : Application
     {
+        //llamo a mi clase DatabaseQuery creandole una variable
+        static DatabaseQuery database;
+
+        public static DatabaseQuery Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    //TestAlumno es el nombre de mi base de datos
+                    database = new DatabaseQuery(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),"TestAlumno.db3"));
+
+                }
+               
+                return database;
+            }
+            
+
+
+        }
+
+
         public App()
         {
             InitializeComponent();
---
+++ MiResidencia20/Data/DatabaseQuery.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MiResidencia20.Models;
+using SQLite;
+
+namespace MiResidencia20.Data
+{
+    public class DatabaseQuery
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        //Creo mi constructor recibe una ruta
+        public DatabaseQuery(string dbPath)
+        {
+            //El contructor recibe un parametro 
+            _database = new SQLiteAsyncConnection(dbPath);
+
+            //le paso un objeto/modelo de Usuario global
+            _database.CreateTableAsync<Usuario>().Wait();
+            
+
+        }
+
+        #region CRUD
+
+        //Declaro una tarea  y una lista recibe parametro de Usuario
+        public Task<List<Usuario>> GetUsuarios()
+        {
+            //Busqueda de Información o Select
+            return _database.QueryAsync<Usuario>("select *from Usuario");
+
+        }
+
+
+        //Insertar Y Actualizar
+
+        public Task<int>SaveUsuarioAsync(Usuario usuario)
+        {
+            if(usuario.Id != 0)
+            {
+                return _database.UpdateAsync(usuario);
+
+            }
+
+            else
+            {
+
+                return _database.InsertAsync(usuario);
+            }
+
+
+        }
+
+        //Eliminar
+        public Task<int> DeleteUsuarioAsync(Usuario usuario)
+        {
+            return _database.DeleteAsync(usuario);
+
+
+        }
+
+
+        #endregion
+
+
+    }
+}
